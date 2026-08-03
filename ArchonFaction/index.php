db25fc5fca10baed71cf8d2326838ecea2e23647
Chombler <64819823+Chombler@users.noreply.github.com>
2026-08-03 02:04:45 -0500

    Correct Archon faction reference data

--- ArchonFaction/index.php
+++ ArchonFaction/index.php
@@ -39,9 +39,9 @@
 	<br/>
 	<p><b>Faction spell</b></p>
 	<p><b><img src="/realm/Factions/picks/Precognition.png" align="middle"> Precognition</b></p>
-	<p><b>Cost</b>: 123,456 Mana - <b>Duration</b>: 60 seconds</p>
+	<p><b>Cost</b>: 123,456 Mana - <b>Duration</b>: 20 seconds</p>
 	<p><b>Effect</b>: Buildings, Assistants, Royal Exchanges, Spells cast and Clicks count more based on Mana produced in this Era.</p>
-	<p><b>Formula</b>: (ln(1 + x) ^ 1.25)%, where x is Mana produced in this Era.</p>
+	<p><b>Formula</b>: (0.65 * ln(1 + x) ^ 1.15)%, where x is Mana produced in this Era. The result is multiplied by at least 1, increasing with spell tier.</p>
 	<p><b>Note</b>: Formula Improved to (1.25 * ln(1 + x) ^ 1.5)% with AR2.</p>
 	<br/>
 	<p><b>Spell Trophy & Upgrade</b></p>
@@ -50,7 +50,7 @@
 	<p><b>Note</b>: Precognition raises regen itself. Make sure that you have 100 Qa (1e17) before being cast.</p>
 	<p><b>Cost</b>: 10 Octg (1e118), A3+ Free</p>
 	<p><b>Effect</b>: A fraction of Precognition's duration is added to time spent in this Era.</p>
-	<p><b>Formula</b>: (100 * ln(1 + x / 60) ^ 2.5), where x is its duration.</p>
+	<p><b>Formula</b>: 150 * ln(1 + x / 60) ^ 2 seconds, where x is its duration in seconds.</p>
 	<hr>
 	<center><b>When using Mercenary</b></center>
 	<p><b>Note</b>: Astral Faction Upgrades can only be purchased with Mercenary Upgrade 4, 8, 12 and 16.</p>
@@ -64,19 +64,19 @@
 	<p><b>Effect</b>: Unlocks Archon Upgrades</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/ArchonUpgrade1.png" align="middle"> Star Trading</b></p>
-	<p><b>Cost</b>: 1 NoQag (1e150)</p>
+	<p><b>Cost</b>: 1e153 Coins</p>
 	<p><b>Effect</b>: Increase the production of all buildings based on the maximum amount of Royal Exchanges you made in this Reincarnation.</p>
 	<p><b>Formula</b>: (0.9 * x)%, where x is the highest amount of Royal Exchanges made in this Reincarnation.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/ArchonUpgrade2.png" align="middle"> Energy Recharge</b></p>
-	<p><b>Cost</b>: 10 NoQag (1e151)</p>
+	<p><b>Cost</b>: 1e155 Coins</p>
 	<p><b>Effect</b>: Maximum Mana counts more based on the amount of time spent as Order in this Reincarnation.</p>
 	<p><b>Formula</b>: (0.25 * x ^ 0.5)%, where x is time spent as Order this Reincarnation</p>
-	<p><b>Effect</b>: Also increase Precognition base, scaling, and changes its starting duration to 5 minutes.</p>
+	<p><b>Effect</b>: Also increase Precognition's base and scaling to (1.3 * ln(1 + x) ^ 1.25)%, and change its starting duration to 1 minute.</p>
 	<p><b>Precognition Formula</b>: (1.25 * ln(1 + x) ^ 1.5)%, where x is Mana produced in this Era.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/ArchonUpgrade3.png" align="middle"> Cosmic Resonance</b></p>
-	<p><b>Cost</b>: 100 NoQag (1e152)</p>
+	<p><b>Cost</b>: 1e157 Coins</p>
 	<p><b>Effect</b>: Increase Royal Exchange bonus based on clicks made in this Reincarnation.</p>
 	<p><b>Formula</b>: +(0.5 * x ^ 0.5)%, where x is clicks made this Reincarnation.</p>
 	<hr>
@@ -86,19 +86,20 @@
 	<p><b>Effect</b>: Unlocks Archon Upgrades</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/ArchonUpgrade4.png" align="middle"> Constellation</b></p>
-	<p><b>Cost</b>: 100 UQig (1e158)</p>
+	<p><b>Cost</b>: 1e163 Coins</p>
 	<p><b>Effect</b>: Multiplicatively increase Mana Regeneration based on the amount of Unique Buildings owned.</p>
 	<p><b>Formula</b>: (0.5 * x ^ 0.5)%, where x is Unique Buildings.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/ArchonUpgrade5.png" align="middle"> Archon Pride</b></p>
-	<p><b>Cost</b>: 1 DQig (1e159)</p>
-	<p><b>Effect</b>: Multiplicatively increase assistants based on your current Lineage level.</p>
-	<p><b>Formula</b>: (x)%, where x is your current Lineage level.</p>
+	<p><b>Cost</b>: 1e165 Coins</p>
+	<p><b>Effect</b>: Multiplicatively increase assistants and Mana produced while offline based on your current Lineage level.</p>
+	<p><b>Assistant Formula</b>: (x)%, where x is your current Lineage level.</p>
+	<p><b>Offline Mana Formula</b>: (1.15 * x ^ 1.15)%, where x is your current Lineage level.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/ArchonUpgrade6.png" align="middle"> Absent-mindedness</b></p>
-	<p><b>Cost</b>: 10 DQig (1e160)</p>
-	<p><b>Effect</b>: Increase Offline bonus based on Excavation Resets made in this Reincarnation.</p>
-	<p><b>Formula</b>: (30 + 3 * (x + y) ^ 3)%, where x and y are free and ruby excavation resets done in this Reincarnation.</p>
+	<p><b>Cost</b>: 1e167 Coins</p>
+	<p><b>Effect</b>: Increase Offline production based on Excavation Depth and Excavation Resets made in this Reincarnation.</p>
+	<p><b>Formula</b>: (30 + 0.75 * x ^ 0.75 + 4 * (1 + y + z) ^ 4)%, where x is Excavation Depth and y and z are free and ruby Excavation Resets made in this Reincarnation.</p>
 	<hr>
 	<p><b>Tier 3 Upgrades</b></p>
 	<p><b><img src="/realm/Factions/picks/ArchonAlliance.png" align="middle"> Archon Alliance</b></p>
@@ -106,19 +107,19 @@
 	<p><b>Effect</b>: Unlocks Archon Upgrades</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/ArchonUpgrade7.png" align="middle"> Superior Consciousness</b></p>
-	<p><b>Cost</b>: 10 TQig (1e163)</p>
+	<p><b>Cost</b>: 1e173 Coins</p>
 	<p><b>Effect</b>: Increase the production of Unique buildings based on their quantity.</p>
 	<p><b>Formula</b>: (x)%, where x is Unique Buildings.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/ArchonUpgrade8.png" align="middle"> Strange Attraction</b></p>
-	<p><b>Cost</b>: 100 TQig (1e164)</p>
+	<p><b>Cost</b>: 1e175 Coins</p>
 	<p><b>Effect</b>: Multiplicatively increase Faction Coin find chance based on the activity time of your least used spell.</p>
 	<p><b>Formula</b>: (100 + 0.8 * x ^ 0.8)%, where x is spell with least activity time this Reincarnation.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/ArchonUpgrade9.png" align="middle"> Arcane Core</b></p>
-	<p><b>Cost</b>: 1 QaQig (1e165)</p>
+	<p><b>Cost</b>: 1e177 Coins</p>
 	<p><b>Effect</b>: Increase the production of all buildings based on mana produced in this Reincarnation.</p>
-	<p><b>Formula</b>: (2.5 * log10(1 + x) ^ 2.5)%, where x is mana produced in this Reincarnation.</p>
+	<p><b>Formula</b>: (2.5 * (0.5 * ln(1 + x)) ^ 2.5)%, where x is mana produced in this Reincarnation.</p>
 	<hr>
 	<p><b><img src="/realm/Factions/picks/ArchonHeritage.png" align="middle"> Archon Heritage</b></p>
 	<p><b>Requirements</b>: Archon Champion Trophy</p>
@@ -129,11 +130,11 @@
 	<p><b><img src="/realm/Factions/picks/NexusQuest.png" align="middle"> Nexus Quest</b></p>
 	<p><b>Description</b>: Hassar, Ruler. Our kind wants to establish better communications with you. Please build more Wizard Towers/Witch Conclaves/Alchemist Labs to convert.</p>
 	<p><b>Requirements</b>: R130+, Buy 25,000 Wizard Towers/Witch Conclaves/Alchemist Labs.</p>
-	<p><b>Cost</b>: 1 QaQig (1e165)</p>
+	<p><b>Cost</b>: 1e177 Coins</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/NexusUpgrade.png" align="middle"> Unique Building Upgrade</b></p>
 	<p><b>Requirements</b>: Nexus Quest.</p>
-	<p><b>Cost</b>: 1 QiQig (1e168)</p>
+	<p><b>Cost</b>: 1e180 Coins</p>
 	<p><b>Effect</b>: Gives Nexus Unique Building.</p>
 	<br/>
 	<p><b>Unique Building</b></p>
@@ -153,17 +154,17 @@
 	<br/>
 	<p><b><img src="/realm/Factions/picks/ArchonUpgrade10.png" align="middle"> Purity of Form</b></p>
 	<p><b>Requirements</b>: Archon Union</p>
-	<p><b>Cost</b>: 100 QiQig (1e170)</p>
+	<p><b>Cost</b>: 1e183 Coins</p>
 	<p><b>Effect</b>: Lineage levels count 200% more for all purposes.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/ArchonUpgrade11.png" align="middle"> Absolute Hierarchy</b></p>
 	<p><b>Requirements</b>: Archon Union</p>
-	<p><b>Cost</b>: 1 SxQig (1e171)</p>
-	<p><b>Effect</b>: Remove 1 Ascension Penalty from Call to Arms.</p>
+	<p><b>Cost</b>: 1e185 Coins</p>
+	<p><b>Effect</b>: All faction spells gain 1 tier.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/ArchonUpgrade12.png" align="middle"> Essence Extractor</b></p>
 	<p><b>Requirements</b>: Archon Union</p>
-	<p><b>Cost</b>: 10 SxQig (1e172)</p>
+	<p><b>Cost</b>: 1e187 Coins</p>
 	<p><b>Effect</b>: Multiplicatively increase production bonus from Gems based on the duration of your longest spell.</p>
 	<p><b>Formula</b>: (0.15 * x ^ 0.7)%, where x is the duration of your longest spell.</p>
 <?php include "../scripts/footer.html"; ?>
