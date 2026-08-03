6c6f058deb03670bce3c903c9dc58f50be650c9c
Chombler <64819823+Chombler@users.noreply.github.com>
2026-08-03 01:55:31 -0500

    Complete Demon faction source audit

--- DemonFaction/index.php
+++ DemonFaction/index.php
@@ -13,9 +13,9 @@
 	<br/>
 	<p><b>Faction spell</b></p>
 	<p><b><img src="/realm/Factions/picks/HellFireBlast.png" align="middle"> Hellfire Blast</b></p>
-	<p><b>Cost</b>: 1000 Mana - <b>Duration</b>: 20 seconds</p>
+	<p><b>Cost</b>: 900 Mana - <b>Duration</b>: 20 seconds</p>
 	<p><b>Effect</b>: Increase the production of Evil Fortresses and Hell Portals based on your amount of trophies.</p>
-	<p><b>Formula</b>: (round(200 * x ^ 0.8))%, where x is your amount of trophies.</p>
+	<p><b>Formula</b>: (120 * x ^ 0.8)%, where x is Trophies unlocked.</p>
 	<br/>
 	<p><b>Spell Trophy & Upgrade</b></p>
 	<p><b><img src="/realm/Factions/picks/HellRushSpellUpgrade.png" align="middle"> Hell Rush</b></p>
@@ -31,17 +31,17 @@
 	<p><b><img src="/realm/Factions/picks/DemonUpgrade1.png" align="middle"> Torture Chambers</b></p>
 	<p><b>Cost</b>: 50 M (5e7)</p>
 	<p><b>Effect</b>: Increase the production of Royal Castles / Evil Fortresses / Iron Strongholds (based on your Alignment) based on the amount of assistants you own.</p>
-	<p><b>Formula</b>: (6 * ln(1 + x) ^ 3)%, where x is the amount of assistants you own.</p>
+	<p><b>Formula</b>: (7 * ln(1 + x) ^ 3.5)%, where x is assistants owned.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/DemonUpgrade2.png" align="middle"> Infernal Magic</b></p>
 	<p><b>Cost</b>: 500 M (5e8)</p>
 	<p><b>Effect</b>: Increase Mana Regeneration based on the number of building tier 10 owned.</p>
 	<p><b>Formula</b>: +(3 + 1.5 * x ^ 0.5), where x is the number of building tier 10 owned.</p>
 	<br/>
-	<p><b><img src="/realm/Factions/picks/DemonUpgrade3.png" align="middle"> Evil Conquerors</b></p>
+	<p><b><img src="/realm/Factions/picks/DemonUpgrade3.png" align="middle"> Burning Legion</b></p>
 	<p><b>Cost</b>: 5 B (5e9)</p>
-	<p><b>Effect</b>: Increase the production of all buildings based on the highest amount of Hell Portals you built in this Reincarnation.</p>
-	<p><b>Formula</b>: (4 * x ^ 0.8)%, where x is the highest amount of Hell Portals you built in this Reincarnation.</p>
+	<p><b>Effect</b>: Gain assistants based on the highest amount of tier-10 Buildings built in this Reincarnation.</p>
+	<p><b>Formula</b>: +(6 + 1.6 * x ^ 0.6), where x is that highest amount.</p>
 	<hr>
 	<p><b>Tier 2 Upgrades</b></p>
 	<p><b><img src="/realm/Factions/picks/DemonFriendshipPact.png" align="middle"> Demons Friendship Pact</b></p>
@@ -55,32 +55,32 @@
 	<p><b><img src="/realm/Factions/picks/DemonUpgrade5.png" align="middle"> Demon Overseers</b></p>
 	<p><b>Cost</b>: 5 T (5e12)</p>
 	<p><b>Effect</b>: Increase the production of the three highest building tiers based on the amount of Trophies you unlocked.</p>
-	<p><b>Formula</b>: (2 * x ^ 0.8)%, where x is the amount of trophies unlocked.</p>
+	<p><b>Formula</b>: (16 + 4 * x ^ 0.8)%, where x is Trophies unlocked.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/DemonUpgrade6.png" align="middle"> Demonic Presence</b></p>
 	<p><b>Cost</b>: 50 T (5e13)</p>
-	<p><b>Effect</b>: Increase the maximum Mana additively based on Evil spells cast in this Era (additive).</p>
-	<p><b>Formula</b>: +(6 * x ^ 0.4), where x is Evil spells cast this Era.</p>
+	<p><b>Effect</b>: Additively increase Maximum Mana based on Evil spells cast in this Reincarnation.</p>
+	<p><b>Formula</b>: +(40 + 4 * ln(1 + x) ^ 4), where x is Evil spells cast in this Reincarnation.</p>
 	<hr>
 	<p><b>Tier 3 Upgrades</b></p>
 	<p><b><img src="/realm/Factions/picks/DemonAlliance.png" align="middle"> Demons Alliance</b></p>
 	<p><b>Cost</b>: 500 Demons Coins</p>
 	<p><b>Effect</b>: Unlocks more Demon upgrades.</p>
 	<br/>
-	<p><b><img src="/realm/Factions/picks/DemonUpgrade7.png" align="middle"> Infernal Magic</b></p>
+	<p><b><img src="/realm/Factions/picks/DemonUpgrade7.png" align="middle"> Devil Tyrant</b></p>
 	<p><b>Cost</b>: 5 Qa (5e15)</p>
-	<p><b>Effect</b>: Hell Portals also increase your Mana Regeneration rate.</p>
-	<p><b>Formula</b>: +(3.5 * x ^ 0.35), where x is the amount of Hell Portals you own.</p>
+	<p><b>Effect</b>: Increase production of the three highest Building tiers based on time spent as Evil this Reincarnation; those tiers also count 100% more.</p>
+	<p><b>Formula</b>: (1.5 * x ^ 0.5)%, where x is time spent as Evil this Reincarnation.</p>
 	<br/>
-	<p><b><img src="/realm/Factions/picks/DemonUpgrade8.png" align="middle"> Burning Legion</b></p>
+	<p><b><img src="/realm/Factions/picks/DemonUpgrade8.png" align="middle"> Evil Conquerors</b></p>
 	<p><b>Cost</b>: 50 Qa (5e16)</p>
-	<p><b>Effect</b>: You gain assistants based on the amount of Hell Portals you own.</p>
-	<p><b>Formula</b>: +(1.5 * x ^ 0.65), where x is the number of Hell Portals you own.</p>
+	<p><b>Effect</b>: Increase all-building production based on the highest amount of Hell Portals built in this Reincarnation.</p>
+	<p><b>Formula</b>: (4 * x ^ 0.8)%, where x is that highest amount.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/DemonUpgrade9.png" align="middle"> Very Bad Guys</b></p>
 	<p><b>Cost</b>: 500 Qa (5e17)</p>
-	<p><b>Effect</b>: Increase the production of Heaven's Gate / Hell Portals / Ancient Pyramid (based on your Alignment) based on the highest amount of Hell Portals you built in this Reincarnation.</p>
-	<p><b>Formula</b>: (4 * x ^ 0.8)%, where x is the highest amount of Hell Portals you built in this Reincarnation.</p>
+	<p><b>Effect</b>: Increase tier-10 Building production based on Hellfire Blast activity time in this Reincarnation.</p>
+	<p><b>Formula</b>: (35 + 0.7 * x ^ 0.7)%, where x is Hellfire Blast activity time in this Reincarnation.</p>
 	<hr>
 	<p><b><img src="/realm/Factions/picks/DemonHeritage.png" alt="Demonic Heritage" align="middle"> Demonic Heritage</b></p>
 	<p><b>Requirements</b>: Demon Champion Trophy</p>
@@ -94,7 +94,7 @@
 	<p><b>Cost</b>: 10 Dtg (1e100)</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/InfernalRealmFactionUpgrade.png" align="middle"> Infernal Realm</b></p>
-	<p>Upgrade Hell Portals To Infernal Realms, boosting their production by 100% per Trophy you unlocked and unlocking more unique perks for the building.</p>
+	<p>Upgrade Hell Portals to Infernal Realms, boosting their production by 40% per Trophy unlocked and unlocking more unique perks for the building.</p>
 	<p><b>Requirements</b>: Buy "Infernal Realm Quest"</p>
 	<p><b>Cost</b>: 10 Ttg (1e103)</p>
 	<p><b>Effect</b>: Also allows access to the Research Facilities.</p>
@@ -135,20 +135,21 @@
 	<p><b>Requirement</b>: Burning Abyss Unique Building</p>
 	<p><b>Cost</b>: 1 Qi (1e18) Demon Coins</p>
 	<p><b>Effect</b>: Grants access to Union Upgrades.</p>
-	<p><b>Effect (R111+)</b>: A fraction of Non-Evil spells cast in this Era count as Evil. Also Trophies count 100% more.</p>
-	<p><b>Formula</b>: (0.01 * x ^ 0.8), where x is Non-Evil spells cast this Era.</p>
+	<p><b>Effect (R105+)</b>: A fraction of Non-Evil spells cast in this Era count as Evil. Trophies also count more based on offline time this Reincarnation.</p>
+	<p><b>Spell Formula</b>: round(0.01 * x ^ 0.8), where x is Non-Evil spells cast this Era.</p>
+	<p><b>Trophy Formula</b>: (30 + 0.3 * y ^ 0.6)%, where y is offline time this Reincarnation.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/DemonUpgrade10.png" alt="Abyssal Furnace" align="middle"> Abyssal Furnace</b></p>
 	<p><b>Cost</b>: 1 Nod (1e60) Emerald Coins</p>
 	<p><b>Requirement</b>: Demons Union</p>
 	<p><b>Effect</b>: Increase the production of all buildings based on the highest amount of Burning Abysses you built in this Reincarnation. Higher tiers get better bonuses.</p>
-	<p><b>Formula</b>: (0.5 * (T ^ 1.5) * (x ^ 0.5)%, where x is the highest amount of Burning Abysses built in this Reincarnation, T is building tier.</p>
+	<p><b>Formula</b>: (0.5 * T ^ 1.5 * x ^ 0.5)%, where x is the highest amount of Burning Abysses built in this Reincarnation and T is Building tier.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/DemonUpgrade11.png" alt="Demonic Fury" align="middle"> Demonic Fury</b></p>
 	<p><b>Requirement</b>: Demons Union</p>
 	<p><b>Cost</b>: 100 Qivg (1e80) Emerald Coins</p>
 	<p><b>Effect</b>: Multiplicatively increase Faction Coin find chance based on least time spent between Evil and Chaos this Reincarnation.</p>
-	<p><b>Formula</b>: (0.25 * min(x , y) ^ 0.75)%, where x is evil time and y is chaos time spent in this Reincarnation.</p>
+	<p><b>Formula</b>: (25 + 0.25 * min(x, y) ^ 0.75)%, where x is Evil time and y is Chaos time this Reincarnation.</p>
 	<br/>
 	<p><b><img src="/realm/Factions/picks/DemonUpgrade12.png" alt="Devastation" align="middle"> Devastation</b></p>
 	<p><b>Requirement</b>: Demons Union</p>
@@ -157,10 +158,10 @@
 	<p><b>Formula</b>: (2.5 * ln(1 + x) ^ 2.5)%, where x is your Mana Regeneration.</p>
 	<hr>
 	<p><b><img src="/realm/Factions/picks/AdvancedDemonHeritage.png" align="middle"> Advanced Demonic Heritage</b></p>
-	<p><b>Requirements</b>: Demon Master trophy and 6 hours activity time (This Era) of Hellfire Blast.</p>
+	<p><b>Requirements</b>: Demon Master trophy and 1 hour activity time (This Era) of Hellfire Blast.</p>
 	<p><b>Cost</b>: 1 Sp (1e24) Demon Coins</p>
 	<p><b>Effect</b>: Increase the production of all buildings based on trophies unlocked.</p>
 	<p><b>Formula</b>: (0.25 * x ^ 0.75)%, where x is amount of trophies unlocked.</p>
 	<p><b>Note</b>: When using their lineage, Advanced Heritage powers are multiplied by the level of the lineage.</p>
-	<p><b>Formula</b>: *(1 + 0.3 * level)</p>
+	<p><b>Base lineage multiplier</b>: *(1 + 0.2 * matching Lineage level).</p>
 <?php include "../scripts/footer.html"; ?>
